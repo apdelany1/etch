acae6b7f0f1b3824afd46dc748898624bbb33cc8
aidan <apdelany1@gmail.com>
2023-01-21 15:58:32 -0500

    created the button to change number of pixels, its going to feed the draw function so we can then update the rows, it will delete the rows before repopulating. then it will update the number of rows on the left hand side. i just have to figure out some corner cases and refactor

--- app.js
+++ app.js
@@ -2,21 +2,11 @@
 const container = document.querySelector(".container");
 let squares;
 draw()
-// // start a for loop creating rows, we will then add cells within the row. 
-// for (let i = 0; i < squares; i++) {
-//     let row = document.createElement("div")
-//     row.classList.add("box");
-//     container.append(row)
-
-//     // this loop will fill in the row we just appened with equal amounts of cells as there are rows
-//     for (let i = 0; i < squares; i++) {
-//         let innerrow = document.createElement("div")
-//         innerrow.classList.add("cell")
-//         row.append(innerrow)
-//     }
-// }
 
 function draw(number = 16) {
+
+    const xbyx = document.querySelector("h2")
+    xbyx.innerText = `${number}x${number}`;
     for (let i = 0; i < number; i++) {
         let row = document.createElement("div")
         row.classList.add("box");
@@ -29,26 +19,21 @@ function draw(number = 16) {
             row.append(innerrow)
         }
     }
-}
+    // point to each cell with qSelectoAll
+    let hov = document.querySelectorAll(".cell");
 
-
-// point to each cell with qSelectoAll
-let hov = document.querySelectorAll(".cell");
-
-// add event listener using forEach
-// hover for CPU
-hov.forEach((div) => {
-    div.addEventListener('mouseover', () => {
-        div.classList.add("fill")
-        console.log("work")
-        // setTimeout(() => {
-        //     div.classList.add("fillBlue");
-        // },2000);
+    // add event listener using forEach
+    // hover for CPU
+    hov.forEach((div) => {
+        div.addEventListener('mouseover', () => {
+            div.classList.add("fill")
+            console.log("work")
+            // setTimeout(() => {
+            //     div.classList.add("fillBlue");
+            // },2000);
+        });
     });
-});
-
-
-
+}
 
 
 
@@ -58,8 +43,13 @@ but.addEventListener("click", () => {
     clear.forEach((div) => {
         div.classList.remove("fill")
     })
-    squares = prompt("How many squares");
-    drawOnReset(squares)
+});
+
+let butt = document.querySelector(".butO");
+butt.addEventListener("click", () => {
+    squares = prompt("How many squares per side?")
+    container.innerHTML = "";
+    draw(squares);
 });
 
 
