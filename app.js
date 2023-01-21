14d9402a195d2c21a46e607d9dc5d21c58b9f578
aidan <apdelany1@gmail.com>
2023-01-21 13:42:08 -0500

    added event listeners and even added a trailing effect to the previously hovered items using set timeout. This ideally will be a mode you can toggle. I will now add components to the sides of the box with information and ability to change number of boxes in grid

--- app.js
+++ app.js
@@ -1,15 +1,42 @@
+// point to the container so we can reference it later
 const container = document.querySelector(".container");
 
-for (let i = 0; i < 5; i++) {
+// start a for loop creating rows, we will then add cells within the row. 
+for (let i = 0; i < 30; i++) {
     let row = document.createElement("div")
     row.classList.add("box");
     container.append(row)
-    for (let i = 0; i < 5; i++) {
+
+    // this loop will fill in the row we just appened with equal amounts of cells as there are rows
+    for (let i = 0; i < 30; i++) {
         let innerrow = document.createElement("div")
         innerrow.classList.add("cell")
         row.append(innerrow)
     }
 }
+
+// point to each sell with qSelectoAll
+const hov = document.querySelectorAll(".cell");
+
+// add event listener using forEach
+// hover for CPU
+hov.forEach((div) => {
+    div.addEventListener('mouseover', () => {
+        div.classList.add("fill")
+
+        setTimeout(() => {
+            div.classList.add("fillBlue");
+        },2000);
+    });
+});
+
+// clicks for mobile
+hov.forEach((div) => {
+    div.addEventListener('click', () => {
+        div.classList.add("fill")
+    });
+});
+
 
 
 
